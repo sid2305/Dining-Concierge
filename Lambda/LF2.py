cce0bc9cbb9ed4e0a23d6cfd81c06e29c313eeb4
Siddhant Lahoti <siddhantlahoti@Siddhants-MBP.lan>
2021-03-06 04:27:23 -0500

    LF2 update

--- Lambda/LF2.py
+++ Lambda/LF2.py
@@ -9,19 +9,20 @@ TABLE_NAME = 'yelp-restaurants'
 SAMPLE_N = '5'
 SEARCH_URL = 'https://search-dineinconcierge-2q6wr7ab7bn5uoqzpbclerolhe.us-west-2.es.amazonaws.com'
 credentials = boto3.Session().get_credentials()
-awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, 'us-west-2', 'es', os.getenv('AWS_SESSION_TOKEN'))
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, 'us-west-2', 'es',session_token=credentials.token)
+# print(credentials.access_key,credentials.secret_key)
 sqs = boto3.resource('sqs',region_name='us-west-2')
 es = Elasticsearch(SEARCH_URL, http_auth=awsauth, connection_class=RequestsHttpConnection)
+# check = es.get(index="restaurants", doc_type="Restaurant", id='-KahGyU9G7JT0JmoC_Yc0Q')
 
-
-def send_sms(number, message):
+def sendsms(number, message):
     # sns = boto3.client(
     #     'sns',
     #     aws_access_key_id=os.getenv('AWS_SERVER_PUBLIC_KEY'),
     #     aws_secret_access_key=os.getenv('AWS_SERVER_SECRET_KEY')
     # )
-    send_sms = boto3.resource('sns', region_name='us-west-2')
-
+    send_sms = boto3.client('sns',region_name='us-west-2')
+    # send_sms = boto3.resource('sns', region_name='us-west-2')
     smsattrs = {
         'AWS.SNS.SMS.SenderID': {
             'DataType': 'String',
@@ -32,7 +33,6 @@ def send_sms(number, message):
             'StringValue': 'Transactional'  # change to Transactional from Promotional for dev
         }
     }
-
     response = send_sms.publish(
         PhoneNumber=number,
         Message=message,
@@ -44,41 +44,59 @@ def send_sms(number, message):
 
 
 def search(cuisine):
-    requestBody = {}
-    requestBody['size'] = SAMPLE_N
-    requestBody['query'] = {}
-    requestBody['query']['bool'] = {}
-    requestBody['query']['bool']['must'] = list([{
-        'match': {
-            'cuisine.title': cuisine
-        }
-    }])
-    data = es.search(index="restaurants", body=requestBody)
+    # requestBody = {}
+    # requestBody['size'] = SAMPLE_N
+    # requestBody['query'] = {}
+    # requestBody['query']['bool'] = {}
+    # requestBody['query']['bool']['must'] = list([{
+    #     'match': {
+    #         'cuisine': cuisine
+    #     }
+    # # }])
+    # data = es.search(index="restaurants", body=requestBody)
+    data = es.search(index="restaurants", body={"query": {"match": {'cuisine':cuisine}}})
+    print("search complete", data['hits']['hits'])
     return data['hits']['hits']
 
 
 def get_restaurant_data(ids):
-    dynamodb = boto3.resource('dynamodb')
-    payload = {}
-    payload[TABLE_NAME] = {
-        "Keys": [{'id': i} for i in ids]
-    }
-    response = dynamodb.batch_get_item(
-        RequestItems=payload
-    )
-
-    res_data = response['Responses'][TABLE_NAME]
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+    table = dynamodb.Table('yelp-restaurants')
+    # all_restaurants = []
     ans = 'Hi! Here are your suggestions,\n '
-    for i in range(0, len(res_data)):
-        ans += "{}. {}, located at {}\n".format(i + 1, res_data[i]['name'], res_data[i]['address'])
-    return ans
+    i = 1
+    for id in ids:
+        if i<6:
+            response = table.get_item(
+                Key={
+                    'restaurantID': id
+                }
+            )
+            response_item = response.get("Item")
+            restaurant_name = response_item['name']
+            # restaurant_category = response_item['category']
+            restaurant_address = response_item['address']
+            restaurant_city = response_item['city:']
+            restaurant_zipcode = response_item['zip_code']
+            restaurant_rating = str(response_item['rating'])
+            # restaurant_url = str(response_item['url'])
+            # restaurant_phone = response_item['phone']
+            # formatted_restaurant_info = restaurant_name + " " + restaurant_phone + '\n' + "Rating: " + restaurant_rating + ", " + restaurant_address + ", " + restaurant_city + ", " + restaurant_zipcode + " " + restaurant_url
+            # all_restaurants.append(formatted_restaurant_info)
+            ans += "{}. {}, located at {}\n".format(i, restaurant_name, restaurant_address)
+            # return ans
+            i += 1
+        else:
+            break
+    print("db pass")
+    return ans # string type
 
 
 def lambda_handler(event=None, context=None):
     queue = sqs.get_queue_by_name(QueueName='restaurant_chatbot')
     messages = queue.receive_messages(MessageAttributeNames=['All'])
-    for msg in messages:
-        print("Received message: %s: %s", msg.message_id, msg.body)
+    # for msg in messages:
+    #     print("Received message: %s: %s", msg.message_id, msg.body)
     # response = sqs.get_queue_url(QueueName='Q1')
     # queue_url = response['QueueUrl']
     # print(queue_url)
@@ -114,10 +132,10 @@ def lambda_handler(event=None, context=None):
         ids = search(cuisine)
         ids = list(map(lambda x: x['_id'], ids))
         rest_details = get_restaurant_data(ids)
-        send_sms("+1"+phone, rest_details)
+        sendsms("+1"+phone, rest_details)
         message.delete()
-    except:
-        print("SQS queue is now empty")
+    except Exception as e:
+        print(e)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda LF2!')
